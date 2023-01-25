5540adad82ed61d590bd27ec6242c3e2dbe65a1d
Stephen Pilaza <christianralphdelacruz@gmail.com>
2023-01-25 20:34:40 -0300

    29-01-2022

---
+++ list.py
@@ -0,0 +1,8 @@
+# List of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+names.append("Nehpets")
+
+names.sort()
+
+print(names)
---
+++ sequences.py
@@ -0,0 +1,11 @@
+name = "Harry"
+
+print(name[0])
+
+name = ["Harry" , "Ron" , "Hermione"]
+
+print(name[0])
+
+coordinate = (10.0, 20.0)
+
+print(coordinate[0])
